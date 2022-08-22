ed5d698766f25060690cebb35796dd3facea0010
WilsonPan <panweipei_2010@163.com>
2022-08-22 08:43:10 +0800

    fix

---
+++ apps/advance/src/App.java
@@ -0,0 +1,8 @@
+import com.apps.advance.Person;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Person p = new Person("Wilson");
+        p.execute();
+    }
+}
