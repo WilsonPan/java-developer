268b99fdd2da6a2890f80348f84597b1bb90f13d
weipei.pan <weipei.pan@yeepay.com>
2025-09-22 16:37:49 +0800

    fix

--- leetcode/maxProfit122.java
+++ leetcode/maxProfit122.java
@@ -30,6 +30,7 @@ public class maxProfit122 {
         for (int i = 1; i < prices.length; i++) {
             maxProfit += Math.max(0, prices[i] - prices[i - 1]);
         }
+        return maxProfit;
 
     }
 
