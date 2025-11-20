b269138abd3a4514eb18640d3a9c4dcf53aa9efc
weipei.pan <weipei.pan@yeepay.com>
2025-11-20 22:46:47 +0800

    mysql local

--- k8s/helm/install.sh
+++ k8s/helm/install.sh
@@ -1,7 +1,1 @@
-kubectl create namespace mysql
-
-kubectl apply -f mysql-local-pvc.yaml
-
-sleep 5
-
 helm install my-mysql bitnami/mysql -f mysql-values.yaml -n mysql --create-namespace --version=9.14.0
